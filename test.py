bbdd675c6d1aeb66bce8dca8e04f01eb904c5077
@josiah-roberts <josiah.david.roberts@gmail.com>
2018-11-07 22:08:12 -0800

    Formatting

--- test.py
+++ test.py
@@ -2,5 +2,13 @@ from operations import rotate, scale, crop, mirror, pipeline, Flip
 from PIL import Image
 
 im = Image.open("kek.jpg")
-rotated = pipeline(im, [rotate(100), scale(1000, 500), crop(100, 100, 400, 400), mirror(Flip.Horizontal), mirror(Flip.Vertical)])
+
+rotated = pipeline(im, [
+    rotate(100), 
+    scale(1000, 500), 
+    crop(100, 100, 400, 400), 
+    mirror(Flip.Horizontal), 
+    mirror(Flip.Vertical)
+])
+
 rotated.show()
