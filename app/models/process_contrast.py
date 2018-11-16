3fd5307485d2f907cb2be3f26d0f676f34cd31c7
ibeckermayer <ibeckermayer@gmail.com>
2018-11-16 00:08:27 -0800

    replaced all swagger_server with app

--- app/models/process_contrast.py
+++ app/models/process_contrast.py
@@ -5,10 +5,10 @@ from datetime import date, datetime  # noqa: F401
 
 from typing import List, Dict  # noqa: F401
 
-from swagger_server.models.base_model_ import Model
-from swagger_server.models.parameter import Parameter  # noqa: F401,E501
-from swagger_server.models.process import Process  # noqa: F401,E501
-from swagger_server import util
+from app.models.base_model_ import Model
+from app.models.parameter import Parameter  # noqa: F401,E501
+from app.models.process import Process  # noqa: F401,E501
+from app import util
 
 
 class ProcessContrast(Model):
--- app/models/processes.py
+++ app/models/processes.py
@@ -5,9 +5,9 @@ from datetime import date, datetime  # noqa: F401
 
 from typing import List, Dict  # noqa: F401
 
-from swagger_server.models.base_model_ import Model
-from swagger_server.models.process import Process  # noqa: F401,E501
-from swagger_server import util
+from app.models.base_model_ import Model
+from app.models.process import Process  # noqa: F401,E501
+from app import util
 
 
 class Processes(Model):
