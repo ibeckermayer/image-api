600a8dbc1ecb47b67fbb5cbac7fa86fb1fa78f21
@josiah-roberts <josiah.david.roberts@gmail.com>
2018-11-15 22:52:35 -0800

    Some more basic request processing

--- app/controllers/default_controller.py
+++ app/controllers/default_controller.py
@@ -1,5 +1,9 @@
 from app import util
 from flask import Flask, request
+from werkzeug.datastructures import FileStorage
+from PIL import Image
+import io
+#from app.models.process import Process
 
 app = Flask(__name__)
 
@@ -11,6 +15,22 @@ def hello():
     if 'Processes' not in request.files:
         return "No Processes specified", 400
     
-    processes = request.files['Processes']
-    print(processes)
+    processes = deserialize(request.files['Processes'])
+    image = loadImage(request.files['Image'])
+
+    return process(processes, image)
+
+def process(processes, image: Image):
     return "WIP", 200
+
+def deserialize(processes: FileStorage):
+    json = processes.stream.read(-1).decode('utf-8')
+    return json
+
+
+def loadImage(imageUpload: FileStorage):
+    in_memory_file = io.BytesIO()
+    imageUpload.save(in_memory_file)    
+    image = Image.open(in_memory_file)
+    image.show()
+    return image
