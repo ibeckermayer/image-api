a3ca7a8df1a61769538e120dfb6137781f327f44
ibeckermayer <ibeckermayer@gmail.com>
2018-11-15 22:17:38 -0800

    trying to upload files

--- app/__main__.py
+++ app/__main__.py
@@ -8,18 +8,12 @@ from app.controllers import default_controller
 SWAGGER_URL = '/image/ui'
 SWAGGER_PATH = 'app/swagger/swagger.yaml'
 
-# app = Flask(__name__)
-
-# @app.route("/image/image-process")
-# def hello():
-#     return "Hello World!"
-
 def main():
     swagger_yml = load(open(SWAGGER_PATH, 'r'), Loader=Loader)
     swaggerui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, SWAGGER_PATH, config={'spec': swagger_yml})
     default_controller.app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
     # app.run(ssl_context='adhoc', port=8080)
-    default_controller.app.run(port=8080)
+    default_controller.app.run(port=5000)
 
 if __name__ == '__main__':
     main()
--- app/controllers/default_controller.py
+++ app/controllers/default_controller.py
@@ -1,23 +1,11 @@
 from app import util
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route("/image/image-process")
+@app.route("/image/image-process", methods=['POST'])
 def hello():
-    return "Hello World!"
-
-
-def image_process_post(image, processes):  # noqa: E501
-    """Takes an image and processes it according to accompanying json.
-
-     # noqa: E501
-
-    :param image: The image.
-    :type image: werkzeug.datastructures.FileStorage
-    :param processes: JSON string containing array of the processes, in the order they should be preformed.
-    :type processes: werkzeug.datastructures.FileStorage
-
-    :rtype: None
-    """
-    return 'do some magic!'
+    if 'file' not in request.files:
+        return "No file"
+    else:
+        return "found file"
