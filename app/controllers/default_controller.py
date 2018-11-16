485d0ee8164c74250cedd56906df4980baa95a3c
ibeckermayer <ibeckermayer@gmail.com>
2018-11-16 00:03:17 -0800

    created a valid example json, successfully converted the string into a dictionary, ready for parsing

--- app/controllers/default_controller.py
+++ app/controllers/default_controller.py
@@ -3,12 +3,13 @@ from flask import Flask, request
 from werkzeug.datastructures import FileStorage
 from PIL import Image
 import io
+import json
 #from app.models.process import Process
 
 app = Flask(__name__)
 
 @app.route("/image/image-process", methods=['POST'])
-def hello():
+def image_process():
     if 'Image' not in request.files:
         return "No Image specified", 400
 
@@ -17,6 +18,8 @@ def hello():
 
     processes = deserialize(request.files['Processes'])
     image = loadImage(request.files['Image'])
+
+    parseJSON(processes)
 
     return process(processes, image)
 
@@ -32,5 +35,9 @@ def loadImage(imageUpload: FileStorage):
     in_memory_file = io.BytesIO()
     imageUpload.save(in_memory_file)
     image = Image.open(in_memory_file)
-    image.show()
+    # image.show()
     return image
+
+def parseJSON(json_string: str):
+    processes = json.loads(json_string)
+    # print(processes)
