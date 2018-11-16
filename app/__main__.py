e0c54ca60176e6a83e10360a473b0835b6e2e6d3
ibeckermayer <ibeckermayer@gmail.com>
2018-11-15 18:06:26 -0800

    swagger ui can now be accessed from server via /image/ui/

--- app/__main__.py
+++ app/__main__.py
@@ -1,14 +1,19 @@
 #!/usr/bin/env python3
 
 from flask import Flask, jsonify
+from flask_swagger_ui import get_swaggerui_blueprint
+from yaml import Loader, load
 
-from app import encoder
-
+SWAGGER_URL = '/image/ui'
+SWAGGER_PATH = 'app/swagger/swagger.yaml'
 
 def main():
-    app = Flask(__name__, static_url_path = "")
-    app.run(port=8080)
-
+    app = Flask(__name__)
+    swagger_yml = load(open(SWAGGER_PATH, 'r'), Loader=Loader)
+    swaggerui_blueprint = get_swaggerui_blueprint(SWAGGER_URL, SWAGGER_PATH, config={'spec': swagger_yml})
+    app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
+    # app.run(ssl_context='adhoc', port=8080)
+    app.run(port=5000)
 
 if __name__ == '__main__':
     main()
