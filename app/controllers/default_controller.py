b5cb8503b69d22cb104aab1c9a4770cbe80084e2
ibeckermayer <ibeckermayer@gmail.com>
2018-11-16 00:16:04 -0800

    imported all of the models into the controller

--- app/controllers/default_controller.py
+++ app/controllers/default_controller.py
@@ -1,9 +1,28 @@
+import io
+import json
 from app import util
 from flask import Flask, request
 from werkzeug.datastructures import FileStorage
 from PIL import Image
-import io
-import json
+from app.models import Parameter
+from app.models import Process
+from app.models import ProcessBlur
+from app.models import ProcessBrightness
+from app.models import ProcessColor
+from app.models import ProcessContrast
+from app.models import ProcessCrop
+from app.models import ProcessEdge
+from app.models import ProcessMaxFilter
+from app.models import ProcessMedianFilter
+from app.models import ProcessMinFilter
+from app.models import ProcessMirror
+from app.models import ProcessModeFilter
+from app.models import ProcessReformat
+from app.models import ProcessRotate
+from app.models import ProcessScale
+from app.models import ProcessSharpen
+from app.models import Processes
+
 #from app.models.process import Process
 
 app = Flask(__name__)
